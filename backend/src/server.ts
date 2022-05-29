e145626f3ac5b9e01b67c90ecc8e664f28a3ee46
Pedro <pedrolucaspalma@gmail.com>
2022-05-29 20:24:16 -0300

    troco

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,8 +8,14 @@ const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(router);
 
 const server = app.listen(PORT, () =>
