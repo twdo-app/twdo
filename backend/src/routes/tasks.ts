132d2f0f1f10f5b34df312c5c28757835ef619e4
Pedro <pedrolucaspalma@gmail.com>
2022-05-29 22:19:07 -0300

    create preliminary task routes

--- backend/src/routes/tasks.ts
+++ backend/src/routes/tasks.ts
@@ -2,4 +2,30 @@ import express from "express";
 
 const router = express.Router();
 
+/**
+ * Get today tasks of the logged user
+ */
+router.get("/today", (req, res) => res.status(200).send({ message: "existe" }));
+
+/**
+ * Create one Task to the logged user
+ */
+router.post("/", (req, res) =>
+    res.status(200).send({ message: "tambem existe" })
+);
+
+/**
+ * Update one Task of the logged user
+ */
+router.put("/:id", (req, res) =>
+    res.status(200).send({ message: "mais um q existe" })
+);
+
+/**
+ * Delete one Task of the logged user
+ */
+router.delete("/:id", (req, res) =>
+    res.status(200).send({ message: "deletado" })
+);
+
 export default router;
