ad35282dfae787d8a1bf8ade5475a7ceee1ef6f7
Pedro <pedrolucaspalma@gmail.com>
2022-05-29 21:56:31 -0300

    create new routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 
 import usersRoutes from "./users";
+import tasksRoutes from "./tasks";
 
 // const tasksRouter = require("./tasks");
 
 const router = express.Router();
 
 router.use("/users", usersRoutes);
-// router.use("/tasks", tasksRouter);
+router.use("/tasks", tasksRoutes);
 
 export default router;
