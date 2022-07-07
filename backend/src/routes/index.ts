7adceefb63f0547b244c2a62505fa27445268871
Pedro <pedrolucaspalma@gmail.com>
2022-07-07 09:19:01 -0300

    add endpoint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,10 +3,12 @@ import express from "express";
 import usersRoutes from "./users";
 import tasksRoutes from "./tasks";
 import projectRoutes from "./projects";
+import utilsRoutes from "./utils";
 
 const router = express.Router();
 
 router.get("/test", (req, res) => res.status(200).send({ message: "foi" }));
+router.use("/utils", utilsRoutes);
 router.use("/users", usersRoutes);
 router.use("/tasks", tasksRoutes);
 router.use("/projects", projectRoutes);
